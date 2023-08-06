5f1bc28ac9b407067ab182059dfab20b16ce0a75
Victory Arogundade <vickycom520@gmail.com>
2023-08-06 19:41:02 +0100

    Changes

--- calc.js
+++ calc.js
@@ -1,18 +1,24 @@
+//This assigns the HTML element 'display' to the JS element 'display'
 let display = document.getElementById("display");
 
+//This is for assigning the arrays of buttons from the HTML element to a single JS element
 let buttons = Array.from(document.getElementsByClassName("button"));
 
+//Uses a switch-case statement to provide functions to various buttons depending on the output of the button
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
+      //Clears screen when clicked
       case "C":
         display.innerText = "";
         break;
+      //Removes the last entry when clicked
       case "←":
         if (display.innerText) {
           display.innerText = display.innerText.slice(0, -1);
         }
         break;
+      //Provides the solution of the display as if a JavaScript statement 
       case "=":
         try {
           display.innerText = eval(display.innerText);
